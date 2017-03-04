67a39adf682a42862fc4605b6e5cb299d4d20437
RIPT1DE <siddiqi.talha1@gmail.com>
2017-03-04 13:00:01 +0500

    Add project files.

---
+++ Tets/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Hello World!")
+        For i = 0 To 10
+            Console.WriteLine(i)
+        Next
+        Dim string12 As String = "    111   sample string"
+        Dim numFound As Boolean
+        For i = 0 To string12.Length - 1
+            If IsNumeric(string12.Chars(i)) And numFound <> True Then
+                Console.WriteLine("Numbers were foud!")
+                numFound = True
+            End If
+        Next
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
